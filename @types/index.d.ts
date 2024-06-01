e74c27231777524ebbb0534e33645ba294f85d19
Vinícius Gutierrez <srgutyerrez@gmail.com>
2024-06-01 13:38:04 -0300

    feat: add message parameteres to exception

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,5 +1,5 @@
 declare module '@x-spacy/exceptions' {
   export class Exception {
-    constructor(status: number, name: string, message?: string);
+    constructor(status: number, name: string, message?: string, parameters?: Record<string, string | number>);
   }
 }
