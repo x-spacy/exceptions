3b36aac2dc5c1c3888aabe236c9cb4e937727ca0
Vinícius Gutierrez <srgutyerrez@gmail.com>
2024-08-26 16:13:17 -0300

    chore: add public properties to exception

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,5 +1,9 @@
 declare module '@x-spacy/exceptions' {
   export class Exception {
+    public readonly status: number;
+    public readonly name: string;
+    public readonly message: string | undefined;
+
     constructor(status: number, name: string, parameters?: Record<string, string | number>, message?: string);
   }
 }
